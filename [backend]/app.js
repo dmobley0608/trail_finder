b761d5b182db5dc93efee097aa4d7dbd6e0e3f92
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2024-01-05 17:03:12 +0000

    initial commit

---
+++ [backend]/app.js
@@ -0,0 +1,29 @@
+//Imports
+const express = require('express')
+const path = require('path')
+
+//GLOBAL VARIABLES
+const PORT = 5000
+
+//Initiliaze app
+const app = express()
+app.use(express.static(path.join(__dirname, "..", "build")))
+app.use(express.static("public"))
+
+//Body Parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//Routes
+
+
+//Forward to frontend
+app.use('/*', (req,res)=>{
+    res.sendFile("index.html", {root: path.join(__dirname,"../build")})
+}) 
+
+
+//Start Server
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})

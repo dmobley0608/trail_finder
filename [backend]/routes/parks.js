8f73410175a26e99ebf8a6c55432c92277e8fe76
dmobley0608 <dmobley0608@gmail.com>
2024-01-06 07:21:02 -0500

    updated park routes for CRUD operations

--- [backend]/routes/parks.js
+++ [backend]/routes/parks.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { getAllParks, addPark } = require('../utilities/database/controller/parks')
+const { getAllParks, addPark, getParkById, editPark, deletePark } = require('../utilities/database/controller/parks')
 
 const router = express.Router()
 
 router.get('/', getAllParks)
+router.get('/:id', getParkById)
 router.post('/', addPark)
+router.put('/:id', editPark)
+router.delete('/:id', deletePark)
 module.exports = router

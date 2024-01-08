6b5b7ce444c333dcc12c0fd44459a01908904bfc
dmobley0608 <dmobley0608@gmail.com>
2024-01-08 05:54:50 -0500

    added signin and registration pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
 import Homepage from './pages/homepage/homepage';
+import Register from './pages/register/register';
+import SignIn from './pages/signIn/signIn';
 
 function App() {
   return (
     <div className="App min-h-dvh max-w-dvw relative">
       <Navbar/>
-      <Homepage/>
+     <Register/>
     </div>
   );
 }

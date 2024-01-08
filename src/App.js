5b2c21e80e456c88634675ea52cc380386f00abb
dmobley0608 <dmobley0608@gmail.com>
2024-01-08 05:17:33 -0500

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar/navbar';
 import Homepage from './pages/homepage/homepage';
 
 function App() {
   return (
     <div className="App min-h-dvh max-w-dvw relative">
+      <Navbar/>
       <Homepage/>
     </div>
   );

18c9032d8d71c810458107e092f0a8bb3e4209f2
dmobley0608 <dmobley0608@gmail.com>
2024-01-08 05:25:02 -0500

    updated z-index

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -17,7 +17,7 @@ function classNames(...classes) {
 export default function Navbar() {
     const user = 'null';
     return (
-        <Disclosure as="nav" className="bg-gray-800 sticky top-0">
+        <Disclosure as="nav" className="bg-gray-800 sticky top-0 z-10">
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">

3f9c8803d57e1e2516359af127ec3968783f3d4b
dmobley0608 <dmobley0608@gmail.com>
2024-01-08 20:02:13 -0500

    implemented loading screen for register and login

--- src/components/navigation/layouts/root/root.jsx
+++ src/components/navigation/layouts/root/root.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from '../../navbar/navbar'
 import { Outlet } from 'react-router'
+import { useGetUserQuery } from '../../../../redux/userAPI'
+import Loading from '../../../../pages/loading/loading'
 
 export default function Root() {
+ 
   return (
-    <>
-    <Navbar/>
-    <Outlet/>
-    </>
+    <div>
+     
+      
+          <Navbar />
+          <Outlet />
+     
+   
+    </div>
+
   )
 }

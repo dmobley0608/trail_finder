29859c6f04a58b4fb6e82afde87fb1a10a84f0f6
dmobley0608 <dmobley0608@gmail.com>
2024-01-16 06:49:04 -0500

    added addressAPI and implemented into store

---
+++ src/redux/addressValidation.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+
+export const addressAPI = createApi({
+    reducerPath:'addressApi',
+    baseQuery:fetchBaseQuery({baseUrl:'https://addressvalidation.googleapis.com/v1:validateAddress'}),
+    endpoints:(builder)=>({
+        findAddress:builder.mutation({
+            query:({...body})=>({
+                url:'',
+                method:'POST',
+                body:body
+            })
+        })
+    })
+})
+
+export const {useFindAddressMutation} = addressAPI
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { parksApi } from "./parksApi";
 import { usersApi } from "./userAPI";
 import { parkReviewsApi } from "./parkReviewsApi";
+import { addressAPI } from "./addressValidation";
 
 
 export const store = configureStore({
-    reducer:{
+    reducer: {
         [usersApi.reducerPath]: usersApi.reducer,
         [parksApi.reducerPath]: parksApi.reducer,
-        [parkReviewsApi.reducerPath]:parkReviewsApi.reducer
-        
+        [parkReviewsApi.reducerPath]: parkReviewsApi.reducer,
+        [addressAPI.reducerPath]: addressAPI.reducer
+
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(parksApi.middleware, usersApi.middleware, parkReviewsApi.middleware),
+        getDefaultMiddleware().concat(parksApi.middleware, usersApi.middleware, parkReviewsApi.middleware, addressAPI.middleware),
 })
